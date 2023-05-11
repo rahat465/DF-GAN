1eb67c1e2d403e454e20d5d997142dd29e27a8ad
rahat465 <105787736+rahat465@users.noreply.github.com>
2023-05-11 11:35:20 +0500

    Update train.sh

--- code/scripts/train.sh
+++ code/scripts/train.sh
@@ -9,8 +9,8 @@ stamp=normal
 train=True
 
 # resume training
-resume_epoch=14
-resume_model_path=./saved_models/coco/base_z_dim100_coco_256_2022_06_04_23_20_33/
+resume_epoch=101
+resume_model_path=./saved_models/coco/pretrained/
 
 # DDP settings
 multi_gpus=True
@@ -18,7 +18,8 @@ nodes=1
 master_port=11111
 
 # You can set CUDA_VISIBLE_DEVICES=0,1,2... to accelerate the training process if you have multiple GPUs
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$nodes --master_port $master_port src/train.py \
+#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$nodes --master_port $master_port src/train.py \
+CUDA_VISIBLE_DEVICES=0 src/train.py \
                     --stamp $stamp \
                     --cfg $cfg \
                     --batch_size $batch_size_per_gpu \
